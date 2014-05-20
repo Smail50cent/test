07238a377d626cf323802dd9976806f3ef7bd0cf
Stagiaire <Stagiaire@stagiaire-02>
2014-05-20 15:32:22 +0200

    Merge origin/master
    Conflicts:
    	gittest

---
+++ service/facade/addAttributCompte.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_GET["id_form"])&&isset($_GET["valeur_champ"])&&isset($_GET["defaut"])&&isset($_GET["id_compte"])) {
+    include_once '../logique/LogiqueFactory.php';
+    $attcompteSrv = LogiqueFactory::getAttributCompteService();
+    $result = $attcompteSrv->addAll($_GET["id_form"],$_GET["valeur_champ"],$_GET["defaut"],$_GET["id_compte"]);
+    echo $result;
+}
