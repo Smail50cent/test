0c21cecfd001c76547a7fb54143a3a3946871baa
damien <contact@damienchesneau.fr>
2014-05-22 15:36:33 +0200

    Ajout style

--- js/database/indexedDB/indexedDBAccesPendingDatas.js
+++ js/database/indexedDB/indexedDBAccesPendingDatas.js
@@ -4,23 +4,23 @@
  */
 
 myStorage.indexedDB.addTicketToBdd = function(method, ticket, param) {
+    var type = config.getConfig("tablePendingDataTypeTicket");
+    log(ticket);
     myStorage.indexedDB.load();
     var request = indexedDB.open(config.getConfig("indexedDBDatabaseName"));
-    log(ticket);
-    var type = config.getConfig("tablePendingDataTypeTicket");
     request.onsuccess = function(e) {
         var db = e.target.result;
         var trans = db.transaction([config.getConfig("tableNamePendingData")], myStorage.IDBTransactionModes.READ_WRITE);
         var store = trans.objectStore(config.getConfig("tableNamePendingData"));
         var request;
-        request = store.put({"type": type, "value": ticket});
+        request = store.put({
+            "id": "menu.id",
+            "value": "menu.nom",
+            "type": "menu.prix"
+        });
         trans.oncomplete = function(e) {
             db.close();
-            if (method != null) {
-                method(param);
-            }
         };
-
         request.onerror = function(e) {
             showErrorMessage(strings.getString("label.error.indexedDB.acces.inpossible"));
         };
