9de81471c5bebf74b55501e79be00198702511e3
damien <contact@damienchesneau.fr>
2014-06-09 16:09:23 +0200

    Ajout redirict pages

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php include_once './header.php'; ?>
 <div id="content_id" class="content content_structure content_personalize">
     <ul id="btn_choice" class=" btn_choice_structure btn_choice_personalize">
-        <li><a href="./paramCommande.php">Passer commande sur place</a></li>
+        <li><a onclick="goClientSurPlace();">Passer commande sur place</a></li>
+        <li><a onclick="goClientAEmporter();">Commande à emporter</a></li>
+        <li><a onclick="goClientLivraison();">Se faire livrer</a></li>
+        <li><a onclick="goClientReservation();">Réserver une table</a></li>
         <li>Les conseils du chef</li>
     </ul>
 </div>
