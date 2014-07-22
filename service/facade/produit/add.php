18bee655f08ac7bd3f9f5fd728a20a3d03444891
Damien Chesneau <contact@damienchesneau.fr>
2014-07-22 13:47:10 +0200

    Add produit

--- service/facade/produit/add.php
+++ service/facade/produit/add.php
@@ -6,21 +6,7 @@ include_once $path . 'service/logique/entity/Categorie.php';
 include_once $path . 'service/logique/entity/SousCategorie.php';
 include_once $path . 'service/logique/entity/Produit.php';
 include_once $path . 'service/logique/entity/AssociationProduitPrix.php';
-$produitSrv = LogiqueFactory::getProduitService();
-$produit = new Produit();
-$produit->setNom("test1");
-$cat = new Categorie();
-$cat->setId(1);
-$produit->setCategorie($cat);
-$souscat= new SousCategorie();
-$souscat->setId(1);
-$produit->setSousCategorie($souscat);
-$produit->setTauxTva("19.6");
-$asso = new AssociationProduitPrix();
-$asso->setDateDebut(null);
-$asso->setDateFin(null);
-$asso->setPrixHt(20);
-$asso->setZoneTable(null);
-$assoPrixProd = array($asso);
-$produit->setAssociationPrixProduit($assoPrixProd);
+/**
+ * TO PHP OBJ !
+ */
 $produitSrv->add($produit);
