385a477152c9387e6d3b163170286f04ab8752b1
LENOVO <hamza.legdani@gmail.com>
2014-07-24 16:01:35 +0200

    add : css class to next prev buttons

--- js/gestion/gestionProduit.js
+++ js/gestion/gestionProduit.js
@@ -283,7 +283,7 @@ function optionPage() {
 
 function submit_optionPage() {
     var list = new Array();
-    $(".ingredOpt_checkbox:checked").each(function() {
+    $(".option_checkbox:checked").each(function() {
         var option = new Option();
         option.setId($(this).attr('id'));
         list.push(option);
@@ -360,36 +360,42 @@ function prixPage() {
 function submit_prixPage() {
     var list = new Array();
     var prix = $("#input_defaut_prix_id").val();
+    var objprix = {prix : prix};
+    var Tva = $("#tva_1").val();
     var associationPrixProduit = new AssociationProduitPrix();
     associationPrixProduit.setDatedebut(null);
     associationPrixProduit.setDatefin(null);
-    associationPrixProduit.setPrixHt(prix);
+    associationPrixProduit.setPrixHt(objprix);
     associationPrixProduit.setZonetable(null);
     list.push(associationPrixProduit);
-    $(".input_zone_prix").each(function() {
-        var associationPPZone = new AssociationProduitPrix();
-        var prix = $(this).val();
-        var idprix = $(this).attr("id_inputprix");
-        var datedebut;
-        var datefin;
-        $(".datetimepicker[id_inputprix=" + idprix + "]").each(function() {
-            if ($(this).hasClass("date_debut")) {
-                var date = $(this).val();
-                datedebut = date.replace('T', ' ');
-            } else if ($(this).hasClass("date_fin")) {
-                var date = $(this).val();
-                datefin = date.replace('T', ' ');
-            }
-
-        });
-        associationPPZone.setDatedebut(datedebut);
-        associationPPZone.setDatefin(datefin);
-        associationPPZone.setPrixHt(prix);
-        associationPPZone.setZonetable(idprix);
-        list.push(associationPPZone);
-    });
+//    $(".input_zone_prix").each(function() {
+//        var associationPPZone = new AssociationProduitPrix();
+//        var prix = $(this).val();
+//        var objprix = {prix : prix};
+//        var idprix = $(this).attr("id_inputprix");
+//        var datedebut;
+//        var datefin;
+//        $(".datetimepicker[id_inputprix=" + idprix + "]").each(function() {
+//            if ($(this).hasClass("date_debut")) {
+//                var date = $(this).val();
+//                datedebut = date.replace('T', ' ');
+//            } else if ($(this).hasClass("date_fin")) {
+//                var date = $(this).val();
+//                datefin = date.replace('T', ' ');
+//            }
+//
+//        });
+//        associationPPZone.setDatedebut(datedebut);
+//        associationPPZone.setDatefin(datefin);
+//        associationPPZone.setPrixHt(objprix);
+//        associationPPZone.setZonetable(idprix);
+//        list.push(associationPPZone);
+//    });
     produit.setAssociationPrixProduit(list);
+    produit.setTauxTva(Tva);
     console.log(produit);
+    var prixTTC = getPrixHtInAssociation(list,produit.getTauxTva());
+    $("#produit_zone_right_prix_id").html(fntp(prixTTC));
     onLoadEtablissementPage();
 }
 
