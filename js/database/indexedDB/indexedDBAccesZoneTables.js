65f72101af62f638ba8e0760561264c19435a395
Damien Chesneau <contact@damienchesneau.fr>
2014-07-31 09:42:18 +0200

    Correction bugs

--- js/database/indexedDB/indexedDBAccesZoneTables.js
+++ js/database/indexedDB/indexedDBAccesZoneTables.js
@@ -72,3 +72,44 @@ myStorage.indexedDB.getAllZoneTables = function(method, param) {
         request.onerror = myStorage.indexedDB.onerror;
     }
 };
+myStorage.indexedDB.getAllZoneTablesByIdEtablissement = function(method, id, param) {
+    entitysFinsh[config.getConfig("tableNameZoneTables")] = false;
+    window.setTimeout(function() {
+        if (entitysFinsh[config.getConfig("tableNameZoneTables")] == false) {
+            impl(method, param);
+        } else {
+            myStorage.indexedDB.getAllZoneTablesByIdEtablissement(method,id, param);
+        }
+    }, delay);
+    function impl(method, param) {
+        myStorage.indexedDB.load();
+        var request = indexedDB.open(config.getConfig("indexedDBDatabaseName"));
+        request.onsuccess = function(e) {
+            var db = e.target.result;
+            var trans = db.transaction([config.getConfig("tableNameZoneTables")], myStorage.IDBTransactionModes.READ_ONLY);
+            var store = trans.objectStore(config.getConfig("tableNameZoneTables"));
+            var keyRange = IDBKeyRange.lowerBound(0);
+            var cursorRequest = store.openCursor(keyRange);
+            var zonesTables = new Array();
+            cursorRequest.onsuccess = function(e) {
+                var result = e.target.result;
+                if (!!result == false) {
+                    return;
+                }
+//                if()Filtrer resultats
+                zonesTables.push(result.value);
+                result.continue();
+            };
+            trans.oncomplete = function(e) {
+                if (method != null) {//Nous avons besoin de l'executer.
+                    method(zonesTables, param);
+                }
+                db.close();
+            };
+            request.onerror = function(e) {
+                showErrorMessage(strings.getString("label.error.indexedDB.acces.inpossible"));
+            };
+        };
+        request.onerror = myStorage.indexedDB.onerror;
+    }
+};
