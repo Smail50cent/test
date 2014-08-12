6d989cb4cd1fe4213b7a22bf6ee8d21ae5c2992b
Damien Chesneau <contact@damienchesneau.fr>
2014-08-12 14:16:00 +0200

    Merge origin/master
    Conflicts:
    	config/strings/string_en_US.xml
    	config/strings/string_fr_FR.xml

--- service/logique/langues/LanguesServiceImpl.php
+++ service/logique/langues/LanguesServiceImpl.php
@@ -1,12 +1,11 @@
 <?php
 
+/**
+ * 
+ * @author Damien Chesneau <contact@damienchesneau.fr>
+ */
 include_once $path . 'service/logique/entity/Compte.php';
 include_once $path . 'service/logique/langues/LanguesService.php';
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 class LanguesServiceImpl implements LanguesService {
 
